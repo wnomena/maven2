ffb01f99a3145cba5b0cb4ec86676bfbe31baff5
wnomena <wnomena58@gmail.com>
2024-01-30 21:44:47 +1200

    javafx

--- MyApp/src/main/java/mada/firstp/App.java
+++ MyApp/src/main/java/mada/firstp/App.java
@@ -1,32 +1,57 @@
 package mada.firstp;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.beans.binding.IntegerBinding;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class App extends Application{
-    private String WINDOW_SETTITLE = "Nomena Walker";
-    @Override
-    public void start(Stage primaryStage){
-        try{
-            primaryStage.initStyle(StageStyle.UNDECORATED);
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("PageDeCalcul.fxml"));
-        Parent root = loader.load();
+     private Label label;
+     private TextField input;
+     private TextField input2;
+     private Button bouton;
+     public IntegerBinding integer;
+     public object value;
 
-        Scene scene = new Scene(root, 800, 400,Color.TRANSPARENT);
-        scene.getStylesheets().add(getClass().getResource("file.css").toString());
-        primaryStage.setTitle(WINDOW_SETTITLE);
+    public void start(Stage primaryStage) throws Exception{
+        VBox root = new VBox();
+        value = new object();
+        root.setAlignment(Pos.CENTER);
+        root.setPadding(new Insets(20));
+        root.setSpacing(20);
+        label = new Label("La somme du calcul actuelle est de  : " + value.getSomme());
+        input = new TextField("5");
+        input2 = new TextField("20");
+        bouton = new Button("Valider");
+        root.getChildren().addAll(label,input,input2,bouton);
+        bouton.setOnAction(new HandleBouton());
+        Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
-        }catch(Exception e){
-            System.out.println("Il y a une erreur quelques part !" + e);
+    }
+
+
+    public static void main(String args0){
+        launch(args0);
+    }
+    public class HandleBouton implements EventHandler<ActionEvent>{
+
+        @Override
+        public void handle(ActionEvent e){
+           try {
+                value.setSomme(Integer.parseInt(input.getText()) + Integer.parseInt(input2.getText()));
+                label.setText("La somme du calcul actuelle est de : " + value.getSomme());
+           } catch (Exception i) {
+            System.out.println(i);
+           }
         }
     }
-    public static void main( String[] args){
-        launch(args);
-    }
-}
+} 
---
+++ MyApp/src/main/java/mada/firstp/object.java
@@ -0,0 +1,30 @@
+package mada.firstp;
+
+public class object {
+
+    
+    private int somme = 0;
+    private int input = 0;
+    private int input2 = 0;
+
+
+    public int getSomme(){
+        return somme;
+    }
+    public int getinput(){
+        return input;
+    }
+    public int getinput2(){
+        return input2;
+    }
+    public void setSomme(int x){
+        this.somme = x;
+    }
+    public void setinput(int x){
+        this.input = x;
+    }
+    public void setinput2(int x){
+        this.input2 = x;
+    }
+}
+
